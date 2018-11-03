5fbc3c955eb4c567c093f283406508e5fe9355a7
Patrick Trierweiler <prt17@my.fsu.edu>
2018-11-03 01:15:16 +0000

    lesson 5 last  commit

--- CurrencyCalculator/currencyCalculator.js
+++ CurrencyCalculator/currencyCalculator.js
@@ -167,7 +167,7 @@ function calculateCurrency(){
         
         
     default:
-       // code block
+    
 }
        
         
@@ -231,9 +231,8 @@ function clearButton () {
         *************************************************************************************************************/            
                 
         //This code resets the dropdown menu to "Australian Dollar (AUD)".
-        document.getElementById("country").value = "0";
-       
-        
+        document.getElementById("country").innerHTML = "0";
+        // wont commit so must add lines
         
         
         //Remove the text from the textfield
